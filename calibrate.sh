4bc39018fd6b63026e0468ab67af18aa5dc23d73
Josh Wu <josh.wu@signify.com>
2020-11-03 17:50:39 +0800

    calibrate: use video10 as front camera
    Signed-off-by: Josh Wu <josh.wu@signify.com>

--- calibrate.sh
+++ calibrate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 run_calibrate_camera.py -i 2 --grid 8x6 --output test.yaml -framestep 20 -r 640x480 --no_gst
+python3 run_calibrate_camera.py -i 10 --grid 8x6 --output front.yaml -framestep 20 -r 1280x720 --no_gst
