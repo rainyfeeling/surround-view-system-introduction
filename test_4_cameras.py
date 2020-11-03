222747854662de2b7c886aecd18c452e528de240
Josh Wu <josh.wu@signify.com>
2020-11-03 10:41:00 +0800

    test_4_cameras: use imshow to show camera video to user
    Signed-off-by: Josh Wu <josh.wu@signify.com>

--- test_4_cameras.py
+++ test_4_cameras.py
@@ -15,9 +15,41 @@ def get_cam_lst(cam_lst=range(0, 24)):
         cap.release()
     return arr
 
+def show_cam_img(cap_device):
+    while True:
+        ret, frame = cap_device.read()
+        if ret:
+            cv2.imshow('video', frame)
+        else:
+            print("failed read frame!")
+
+        c = cv2.waitKey(1)
+        if c == ord('q'):
+            break
+
+    cv2.destroyAllWindows()
+
+def init_caps(cam_list):
+    caps = []
+    for iCam in cam_list:
+        cap = cv2.VideoCapture(iCam)
+        caps.append(cap)
+
+    return caps
+
+def deinit_caps(cap_list):
+    for cap in cap_list:
+        cap.release()
+
 def main():
     cam_list = get_cam_lst([2, 6, 10, 14])
     print("avaliable list is {}".format(cam_list))
+
+    caps = init_caps(cam_list)
+
+    show_cam_img(caps[0])
+
+    deinit_caps(caps)
 
 if __name__ == "__main__":
     main()
