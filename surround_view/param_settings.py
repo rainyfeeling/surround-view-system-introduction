5b5bf5679202429404ba779b6dfc69d6e8ac9473
Josh Wu <josh.wu@signify.com>
2020-12-07 18:04:06 +0800

    param_settings: add more data in comment
    Signed-off-by: Josh Wu <josh.wu@signify.com>

--- surround_view/param_settings.py
+++ surround_view/param_settings.py
@@ -16,11 +16,13 @@ inn_shift_w = 10
 inn_shift_h = 10
 
 # total width/height of the stitched image
+# total_w = 620, total_h = 580
 total_w = 420 + 2 * shift_w
 total_h = 380 + 2 * shift_h
 
 # four corners of the rectangular region occupied by the car
 # top-left (x_left, y_top), bottom-right (x_right, y_bottom)
+# xl = 210, xr = 410, yt = 210, yb = 370
 xl = shift_w + 100 + inn_shift_w
 xr = total_w - xl
 yt = shift_h + 100 + inn_shift_h
@@ -28,6 +30,7 @@ yb = total_h - yt
 # --------------------------------------------------------------------
 
 project_shapes = {
+    # front - (620 x 210)
     "front": (total_w, yt),
     "back":  (total_w, yt),
     "left":  (total_h, xl),
@@ -38,6 +41,7 @@ project_shapes = {
 # you must click these pixels in the same order when running
 # the get_projection_map.py script
 project_keypoints = {
+    # [_topleft(200, 100), _topright(420, 100), _bottomleft(200, 200), _bottomright(420, 200)]
     "front": [(shift_w + 100, shift_h),
               (shift_w + 320, shift_h),
               (shift_w + 100, shift_h + 100),
